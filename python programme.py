ea4aff7bf9f1e61cfc9d13760caa5e57edfd8c90
kreepabaid <77733719+kreepabaid@users.noreply.github.com>
2021-01-20 18:52:54 +0530

    Add files via upload

---
+++ python programme.py
@@ -0,0 +1,45 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("hello")
+hello
+>>> print("print your name")
+print your name
+>>> n=input()
+kreepa
+>>> print("welcome your name is:"+n)
+welcome your name is:kreepa
+>>> print("what is your age?")
+what is your age?
+>>> a=input()
+14
+>>> print("your age is:"+a)
+your age is:14
+>>> print("what is your hobby?")
+what is your hobby?
+>>> b=input()
+Dancing
+>>> print("your hobby is :"+b)
+your hobby is :Dancing
+>>> print("Welcome to the supermarket.PLease enter your name.")
+Welcome to the supermarket.PLease enter your name.
+>>> c=input()
+Kreepa Baid
+>>> print("welcome"+c)
+welcomeKreepa Baid
+>>> print("Welcome " +c+ " how may I help you?" )
+Welcome Kreepa Baid how may I help you?
+>>> print("1. VEgetables")
+1. VEgetables
+>>> print("2. Fruits")
+2. Fruits
+>>> print("3. Snacks")
+3. Snacks
+>>> print("4. Dairy products")
+4. Dairy products
+>>> print("What is your choice?")
+What is your choice?
+>>> p=input()
+1
+>>> print("your choice is :"+p)
+your choice is :1
+>>> 
